7567b4909a68236a545f47ca8c24eb703b8124d2
Silva01 <caue.alves.silva@gmail.com>
2025-05-05 14:47:56 -0300

    #

---
+++ src/SistemaMedida.java
@@ -0,0 +1,14 @@
+public class SistemaMedida {
+    String sigla = "M";
+
+    if(sigla == "p")
+        System.out.println("PEQUENO");
+        else if(sigla == "M")
+        System.out.println("MEDIO");
+        else if(sigla == "G")
+        System.out.println("GRANDE");
+        else
+        System.out.println("INDF");
+        
+    
+}
